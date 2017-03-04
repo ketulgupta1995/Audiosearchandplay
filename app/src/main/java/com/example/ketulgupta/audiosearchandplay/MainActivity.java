5f63f3f7ba703da58ac5eaff2c53c92b5c03a235
Ketul Gupta <ketulgupta1995@gmail.com>
2017-03-04 14:14:30 +0530

    Made a simple audio player(.mp3)

--- app/src/main/java/com/example/ketulgupta/audiosearchandplay/MainActivity.java
+++ app/src/main/java/com/example/ketulgupta/audiosearchandplay/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ketulgupta.audiosearchandplay;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +16,13 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.jar.Manifest;
@@ -23,44 +31,63 @@ public class MainActivity extends AppCompatActivity {
     private ListView mListView;
     String[] Countries = new String[]{"India", "Australia", "Newzealand",
             "Indonesia", "China", "Russia", "Japan", "South Korea"};
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},255);
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 255);
         mListView = (ListView) findViewById(R.id.list_view);
         Log.d("S", Environment.getExternalStorageDirectory().toString());
-        File rootFolder=Environment.getExternalStorageDirectory();
+        File rootFolder = Environment.getExternalStorageDirectory();
 //        File downloads=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
 //        Log.d("S", Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString());
-        List<File >allFiles =getListFiles(rootFolder);
-//        for (int i=0;i<allFiles.size();i++)
-//        {
-//            Log.d("F",allFiles.get(i).getName());
-//        }
-        ArrayAdapter<String> countryAdapter =new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,Countries);
+        List<File> allFiles = getListFiles(rootFolder);
+        final String[] theNamesOfFiles = new String[allFiles.size()];
+
+        for (int i=0;i<allFiles.size();i++)
+        {
+            theNamesOfFiles[i]=allFiles.get(i).getPath();
+        }
+        ArrayAdapter<String> countryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, theNamesOfFiles);
+//        ArrayAdapter<String> countryAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Countries);
         mListView.setAdapter(countryAdapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this,Countries[i],Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, Countries[i], Toast.LENGTH_SHORT).show();
+//                String filePath = Environment.getExternalStorageDirectory()+"/yourfolderNAme/yopurfile.mp3";
+                MediaPlayer mediaPlayer = new  android.media.MediaPlayer();
+                try {
+                    mediaPlayer.setDataSource(theNamesOfFiles[i]);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                }
+                catch (IOException e){
+                    Log.d("IO",e.toString());
+                }
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private List<File> getListFiles(File parentDir){
+    private List<File> getListFiles(File parentDir) {
         List<File> inFiles = new ArrayList<>();
-        File [] files= parentDir.listFiles();
+        File[] files = parentDir.listFiles();
         for (File file : files) {
-            if (file.isDirectory())
-            {
+            if (file.isDirectory()) {
                 inFiles.addAll(getListFiles(file));
-            }
-            else {
-                if(file.getName().endsWith(".mp3")){
+            } else {
+                if (file.getName().endsWith(".mp3")) {
                     inFiles.add(file);
-                    Log.d("F",file.getName());
+                    Log.d("F", file.getName());
                 }
             }
         }
@@ -68,4 +95,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
